44a4ff8bd424ae12b4276925e3fc9f11f62741dd
Git Server Admin <admin@git-server>
2020-01-12 22:33:12 +0000

    Add initial repository

---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'timer',
+  templateUrl: './timer.component.html',
+})
+export class TimerComponent {
+
+  started: boolean;
+  minutes: number;
+  seconds: number;
+  newMin: number;
+  interval: any;
+
+  constructor() {
+  }
+
+  resetVariables(mins, secs, started) {
+    
+  }
+
+  start() {
+    
+  }
+                                                                      
+  addFive() {
+    
+  }
+
+  minusFive() {
+    
+  }
+
+
+  stop() {
+    
+  }
+
+  reset() {
+
+  }
+
+  intervalCallback() {
+    
+  }
+
+}
