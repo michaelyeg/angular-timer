d6aa4fa9702b257ba0af04f2d11ed0a2c1622849
michaelHottomali <michael@hottomali.com>
2022-06-09 21:23:48 -0700

    Passed all unit tests

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-timer',
   templateUrl: './timer.component.html',
 })
-export class TimerComponent {
+export class TimerComponent implements OnInit {
   started: boolean;
   minutes: number;
   seconds: number;
@@ -24,28 +24,49 @@ export class TimerComponent {
   }
 
   start() {
-    this.started = true;
+    if (!this.started && !(this.minutes === 0 && this.seconds === 0)) {
+      this.started = true;
+      this.interval = setInterval(() => this.intervalCallback(), 1000);
+    }
   }
 
   addFive() {
-    this.minutes += 5;
+    if (this.started) {
+      this.minutes += 5;
+    }
   }
 
   minusFive() {
-    this.minutes -= 5;
+    if (this.started) {
+      this.minutes -= 5;
+    }
   }
 
-
   stop() {
+    clearInterval(this.interval);
     this.started = false;
   }
 
   reset() {
-    this.resetVariables(25,0, false);
+    this.stop();
+    this.resetVariables(25, 0, false);
   }
 
   intervalCallback() {
-
+    if (this.minutes < 0) {
+      this.stop();
+      this.resetVariables(0, 0, false);
+    } else if (this.seconds > 0) {
+      this.seconds --;
+    } else if (this.seconds === 0 && this.minutes > 0) {
+      this.minutes -= 1;
+      this.seconds = 59;
+    } else {
+      this.stop();
+    }
   }
 
+  ngOnInit(): void {
+    this.reset();
+  }
 }
